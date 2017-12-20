3d7b2d89e6b29b28d035d1905252ce6200925e3b
rsaless <rspavarina@gmail.com>
2017-12-20 01:34:22 -0200

    Adicionado OtaSplashScreenActivity

--- app/src/main/java/com/ota/otaagenda/otaagenda/Splashscreen/OtaSplashScreenActivity.java
+++ app/src/main/java/com/ota/otaagenda/otaagenda/Splashscreen/OtaSplashScreenActivity.java
@@ -1,19 +1,18 @@
 package com.ota.otaagenda.otaagenda.Splashscreen;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import com.ota.otaagenda.otaagenda.Login.LoginActivity;
-import com.ota.otaagenda.otaagenda.Main.MainActivity;
 import com.ota.otaagenda.otaagenda.R;
 
-public class SplashscreenActivity extends AppCompatActivity {
+public class OtaSplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.activity_ota_splash_screen);
 
         Thread timer= new Thread() {
             public void run() {
@@ -22,16 +21,17 @@ public class SplashscreenActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally{
-                    openLogin();
+                    openSplash();
                 }
             }
         };
         timer.start();
 
+
     }
 
-    protected void openLogin(){
-        Intent intentLogin = new Intent(SplashscreenActivity.this, LoginActivity.class);
+    protected void openSplash(){
+        Intent intentLogin = new Intent(OtaSplashScreenActivity.this, SplashscreenActivity.class);
         startActivity(intentLogin);
     }
 
@@ -40,5 +40,4 @@ public class SplashscreenActivity extends AppCompatActivity {
         super.onPause();
         finish();
     }
-
 }
